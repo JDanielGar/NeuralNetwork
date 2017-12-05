b578ddd2ccd9f6c2cbee2226d003975ea41686e2
Daniel Garcia <waspc4d@hotmail.com>
2017-12-05 03:31:34 -0500

    Add softmax and scores

--- E-Commerce/prediction.py
+++ E-Commerce/prediction.py
@@ -16,6 +16,13 @@ V = np.random.randn(H, K) # Weights Two
 c = np.random.randn(1, K) # Bias Two
 
 """
+Classification Rate | Acuracy beetween the predictions 
+"""
+
+def classification_rate(Y, P):
+    return np.mean(Y == P)
+
+"""
 Forward data into the network
 Second part of the project
 
@@ -28,17 +35,66 @@ Part One:
 def sigmoid(X):
     return 1 / ( 1 + np.exp(-X) )
 
-def forward(W, X, b):
+def forward_sigmoid(W, X, b):
     return sigmoid(X.dot(W) + b)
 
 # Runing functions
 
-z = forward(W, X, b) # First Layer ->
+z = forward_sigmoid(W, X, b) # First Layer ->
 
-p = forward(V, z, c) # Second Layer ->
+p = forward_sigmoid(V, z, c) # Second Layer ->
+
+final_prediction = np.argmax(p, axis=1)
+
+print(classification_rate(Y, final_prediction))
 
 """
         Analising the results:
             - The sum overall the dimensions
               of one sample is more than one.
+
+Part Two:
+    Softmax
+"""
+
+# Defining functions
+
+def softmax(X):
+    return np.exp(X) / np.exp(X).sum(axis=1, keepdims=True)
+
+def forward_softmax(W, X, b):
+    return softmax(X.dot(W) + b )
+
+# Runing functions
+
+z = forward_softmax(W, X, b) # First Layer ->
+
+p = forward_softmax(V, z, c) # Second Layer ->
+
+final_prediction = np.argmax(p, axis=1)
+
+print(classification_rate(Y, final_prediction))
+
+"""
+        Analising the results:
+            - The sum overall the dimensions
+              of one sample is one.
+
+Part Three:
+    Sigmoid and Softmax
+"""
+
+# Runing functions
+
+z = forward_sigmoid(W, X, b) # First Layer ->
+
+p = forward_softmax(V, z, c) # Second Layer ->
+
+final_prediction = np.argmax(p, axis=1)
+
+print(classification_rate(Y, final_prediction))
+
+"""
+        Conclusion:
+        More acurracy with only softmax?
 """
